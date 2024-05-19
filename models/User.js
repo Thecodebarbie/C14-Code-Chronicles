7bd7e755c6b458e7aad6c438bdd2a8f732ff435b
THECODEBARBIE <153017074+THEGRLCODE@users.noreply.github.com>
2024-05-19 17:34:35 -0400

    Added attributes to the User model including id, username, and password with corresponding data types and constraints.

--- models/User.js
+++ models/User.js
@@ -7,3 +7,27 @@ class User extends Model {
     return bcrypt.compareSync(loginPw, this.password);
   }
 }
+
+User.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [8]
+        }
+      }
+    },
+
+  );
+  
