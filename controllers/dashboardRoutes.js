e3db0dac53a97dd269349973e7e94a00d4a143bc
THECODEBARBIE <153017074+THEGRLCODE@users.noreply.github.com>
2024-05-20 08:07:31 -0400

    feat: Add dashboard route with authentication middleware
    Implemented a new route to fetch user data and their associated posts for the dashboard view. Used withAuth middleware to ensure only authenticated users can access the dashboard. The user's data, excluding the password, is rendered along with their posts on the dashboard.

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -1,3 +1,22 @@
 const router = require('express').Router();
 const { Post, User } = require('../models');
 const withAuth = require('../utils/auth');
+
+router.get('/', withAuth, async (req, res) => {
+    try {
+      const userData = await User.findByPk(req.session.user_id, {
+        attributes: { exclude: ['password'] },
+        include: [{ model: Post }],
+      });
+  
+      const user = userData.get({ plain: true });
+  
+      res.render('dashboard', {
+        ...user,
+        logged_in: true,
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
