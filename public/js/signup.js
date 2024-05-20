0efe50c59fbe6f41c4e17b9968d8147a04595d10
THECODEBARBIE <153017074+THEGRLCODE@users.noreply.github.com>
2024-05-20 09:12:00 -0400

    didnt need signup

--- public/js/signup.js
+++
@@ -1,8 +0,0 @@
-const signupFormHandler = async (event) => {
-    event.preventDefault();
-  
-    const username = document.querySelector('#username-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
-  
-  };
-  
