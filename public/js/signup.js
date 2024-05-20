da62b20b748bcbaa9992dcd43e0049a873cd0be4
THECODEBARBIE <153017074+THEGRLCODE@users.noreply.github.com>
2024-05-20 09:10:05 -0400

    Add signup functionality with form handler in signup.js

---
+++ public/js/signup.js
@@ -0,0 +1,8 @@
+const signupFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const username = document.querySelector('#username-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+  
+  };
+  
